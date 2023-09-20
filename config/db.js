6cf9fd0bfc1ae799747687734693b9f4b8dfd6eb
Akshay <akshayrathee902@gmail.com>
2023-09-20 21:45:37 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
-import colors from "colors";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Connected To MongoDB Database ${conn.connection.host}`.bgMagenta.white
-    );
+    console.log(`Connected To MongoDB Database ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error in MongoDB ${error}`.bgRed.white);
+    console.log(`Error in MongoDB ${error}`);
   }
 };
 
--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 import express from "express";
-import colors from "colors";
+
 import dotenv from "dotenv";
-import morgan from "morgan";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
-import {fileURLToPath} from "url";
 
 dotenv.config();
 
 connectDB();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRoutes);
@@ -35,7 +30,5 @@ app.use("*", function (req, res) {
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(
-    `server runing on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
-  );
+  console.log(`server runing on ${process.env.DEV_MODE} mode on ${PORT}`);
 });
